7905258b80349468208b6c7f8f6b5dc1021588a2
Dan Radenkovic <bogdan.radenkovic@gmail.com>
2018-07-01 19:57:32 +0200

    basic S3 Uploader

---
+++ src/file-upload/index.js
@@ -0,0 +1,63 @@
+// @flow
+import { S3 } from 'aws-sdk';
+
+type UploadConfig = {
+  s3: {
+    accessKeyId: string,
+    secretAccessKey: string,
+    region: string
+  },
+  bucket: string,
+  urlExpiration?: number
+};
+
+type UploadData = {
+  file: Buffer,
+  key: string,
+  success?: Function,
+  error?: Function
+};
+
+export default class FileUpload {
+  constructor(config: UploadConfig) {
+    this.config = config;
+    this.checkConfig();
+    this.s3 = new S3(config.s3);
+  }
+
+  checkConfig() {
+    if (!this.config) throw new Error('Configuration object not supplied');
+    if (!this.config.s3)
+      throw new Error('Configuration object needs s3 params');
+    if (!this.config.bucket)
+      throw new Error('Configuration requires bucket property');
+  }
+
+  upload(data: UploadData) {
+    // Check extensions
+    function callback(err, response) {
+      if (err && data.error) data.error(err);
+      if (response && data.success) {
+        const url = this.getUrl(data.key);
+        data.success(url);
+      }
+    }
+    return this.s3.putObject(
+      {
+        Bucket: this.config.bucket,
+        Key: data.key,
+        Body: data.file
+      },
+      callback.bind(this)
+    );
+  }
+
+  getUrl(key: string): string {
+    const params = {
+      Bucket: this.config.bucket,
+      Key: key,
+      Expires: this.config.urlExpiration || 120
+    };
+    return this.s3.getSignedUrl('getObject', params);
+  }
+}
---
+++ src/file-upload/test/file-upload.test.js
@@ -0,0 +1,62 @@
+// @flow
+import fs from 'fs';
+import FileUpload from '../index';
+
+const Uploader = new FileUpload({
+  s3: {
+    accessKeyId: 'test',
+    secretAccessKey: 'test',
+    region: 'us-east-1'
+  },
+  bucket: 'toolbelt'
+});
+
+describe('Upload to S3', () => {
+  const file = fs.readFileSync(
+    `${process.cwd()}/src/file-upload/test/sample.txt`
+  );
+
+  // Mock S3 Uploader
+  Uploader.s3.putObject = (params, callback) => {
+    if (params.Body) return callback(null, true);
+    return callback(true);
+  };
+
+  test('Upload success', () => {
+    Uploader.upload({
+      file,
+      key: 'test/sample.txt',
+      success: url => expect(url).toBeDefined(),
+      error: err => expect(err).toBeUndefined()
+    });
+  });
+
+  test('Upload failure', () => {
+    Uploader.upload({
+      key: 'test/sample.txt',
+      success: url => expect(url).toBeUndefined(),
+      error: err => expect(err).toBeDefined()
+    });
+  });
+});
+
+describe('Configuration object', () => {
+  test('No configuration exception', () => {
+    expect(() => {
+      const ShouldFail = new FileUpload();
+      return ShouldFail;
+    }).toThrow();
+  });
+  test('No s3 configuration exception', () => {
+    expect(() => {
+      const ShouldFail = new FileUpload({});
+      return ShouldFail;
+    }).toThrow();
+  });
+  test('No bucket exception', () => {
+    expect(() => {
+      const ShouldFail = new FileUpload({ s3: {} });
+      return ShouldFail;
+    }).toThrow();
+  });
+});
